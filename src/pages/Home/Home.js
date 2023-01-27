385aec71b7d17a0dbe26f64807623f22c9a9be1b
Waseem Khalo <waseemkhalo@Waseems-Air.lan>
2023-01-27 11:47:00 -0500

    edit change

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,13 @@
 import Navbar from "../../components/Navbar/Navbar";
 import './Home.scss'
 
-function Home () {
+function Home() {
 
     return (
         <div className="home">
+            <Navbar />
             <h1 className="home__heading">Waseem Khalo</h1>
             <h2 className="home__subheading">🚜</h2>
-            {/* <Navbar /> */}
         </div>
     );
 }
