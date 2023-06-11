9eafc6b9b21ea31edaaa1d141e79704ebf41b1f0
Waseem <waseemkhalo@gmail.com>
2023-06-11 15:56:57 -0400

    add pfp & heading

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,6 +3,10 @@ import "./Hero.scss";
 import pfp from "../../assets/pfp.jpg";
 import icons from "../../assets/icons/Web icons.png";
 import waseem from "../../assets/icons/W.png";
+import back from "../../assets/icons/back.svg";
+import forward from "../../assets/icons/forward.svg";
+import refresh from "../../assets/icons/refresh.svg";
+import SSL from "../../assets/icons/lock.svg";
 
 function Hero() {
   return (
@@ -13,16 +17,20 @@ function Hero() {
         <h5 className="hero__icons--waseem">Waseem Portfolio |</h5>
       </div>
       <div className="hero__searchbar">
-        <img className="hero__searchbar--icons"/>
+        <img src={back} className="hero__icons--back" />
+        <img src={forward} className="hero__icons--forward" />
+        <img src={refresh} className="hero__icons--refresh" />
         <div className="hero__searchbar--search"></div>
-        <img className="hero__searchbar--SSL" />
+        <img src={SSL} className="hero__icons--SSL" />
         <h6 className="hero__searchbar--url">waseemkhalo.com/</h6>
       </div>
-      <img className="hero__pfp" src={pfp} />
-      <h1 className="hero__heading">Waseem Khalo</h1>
-      <h2 className="hero__subheading">testing 123🚜</h2>
+      <div className="hero__bio">
+        <img className="hero__pfp" />
+        <h1 className="hero__heading">Waseem Khalo</h1>
+        <h2 className="hero__subheading">testing 123🚜</h2>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero;
