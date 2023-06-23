d521e6b64840ddafcbf2333dbd21ca7771da45d3
Waseem <waseemkhalo@gmail.com>
2023-06-23 13:12:40 -0400

    add alt

--- src/components/Apps/Apps.js
+++ src/components/Apps/Apps.js
@@ -10,9 +10,10 @@ function Apps() {
         className="apps__link"
         target="_blank"
         href="https://www.everydaytodo.com"
+        rel="noopener noreferrer"
       >
         <div className="apps__container">
-          <img className="apps__logo" src={everyday} />
+          <img className="apps__logo" src={everyday} alt='everyday logo'/>
         </div>
         <h4 className="apps__header">Everyday</h4>
       </a>
@@ -20,9 +21,10 @@ function Apps() {
         className="apps__link"
         target="_blank"
         href="https://www.everydaytodo.com"
+        rel="noopener noreferrer"
       >
         <div className="apps__container">
-          <img className="apps__logo" src={anbu} />
+          <img className="apps__logo" src={anbu} alt='anbu logo' />
         </div>
         <h4 className="apps__header">Anbu</h4>
       </a>
