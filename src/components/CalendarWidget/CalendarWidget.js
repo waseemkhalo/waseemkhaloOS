5777b9e15eecd86d1b2a88bf84d033f18ef5df73
Waseem <waseemkhalo@gmail.com>
2023-07-20 17:27:49 -0400

    add calendar widget

---
+++ src/components/CalendarWidget/CalendarWidget.js
@@ -0,0 +1,27 @@
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import "./CalendarWidget.scss";
+
+function CalendarWidget() {
+  return (
+    <div className="calendar">
+      <div className="calendar__container">
+        <h1 className="calendar__title">Today</h1>
+        <div className="calendar__meetings-container">
+          <p className="calendar__meetings">1:00 PM l Finish this portfolio </p>
+          <p className="calendar__meetings">1:30 PM l Create new projects </p>
+          <p className="calendar__meetings">2:30 PM l Learn new skills </p>
+          <p className="calendar__meetings">3:30 PM l Get a job</p>
+          <p className="calendar__meetings">4:30 PM l Create impactful products </p>
+        </div>
+      </div>
+
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar />
+      </LocalizationProvider>
+    </div>
+  );
+}
+
+export default CalendarWidget;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import Apps from "../../components/Apps/Apps";
 import "./Home.scss";
 import Hero from "../../components/Hero/Hero";
 import Hangar from "../../components/Hangar/Hangar";
+import CalendarWidget from "../../components/CalendarWidget/CalendarWidget";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <div className="home">
       <Hero />  
         <div className="home__right-side">
+          <CalendarWidget />
           <Apps />
         </div>
       </div>
