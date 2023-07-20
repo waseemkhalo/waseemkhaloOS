81611528f9b4930e3653b377fcf1bded03067c1a
Waseem <waseemkhalo@gmail.com>
2023-07-20 15:48:56 -0400

    add date

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,35 @@
-import "./Navbar.scss"
-import mic from '../../assets/icons/microphone.svg'
+import "./Navbar.scss";
+import mic from "../../assets/icons/microphone.svg";
+import w from '../../assets/icons/W.png'
 
 function Navbar() {
 
-    return (
+    // get current date function MONTH DD, YYYY
+    const date = new Date().toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    });
 
-        <div className='navbar'>
-            <h1 className="navbar__prompt">Try “Hey Blue, Open Email”</h1>
-            <img className="navbar__mic" src={mic}/> 
+  return (
+    <div className="navbar">
+      <div className="navbar__prompt">
+        <h1 className="navbar__search">
+          Try “Hey Seemo, Show me your portfolio”
+        </h1>
+        <img className="navbar__mic" src={mic} alt="microphone img"/>
+      </div>
+
+      <div>
+        <img src={w} alt="waseem logo" />
+        <div>
+            <h6>{date}</h6>
+            <h5></h5>
         </div>
-    );
-  }
-  
-  export default Navbar;
-  
+        
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
