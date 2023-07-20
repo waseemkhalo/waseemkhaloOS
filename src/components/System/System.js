eb3daa2852da53391d56e39ec63165237081d295
Waseem <waseemkhalo@gmail.com>
2023-07-20 17:33:22 -0400

    update time and date every second

--- src/components/System/System.js
+++ src/components/System/System.js
@@ -1,22 +1,44 @@
-import './System.scss';
+import "./System.scss";
 import w from "../../assets/icons/w-large.png";
 import bluetooth from "../../assets/icons/bluetooth.svg";
 import wifi from "../../assets/icons/wifi.svg";
 import battery from "../../assets/icons/battery.svg";
+import { useState, useEffect } from "react";
 
 function System() {
-  // get current date function MONTH DD, YYYY
-  const date = new Date().toLocaleDateString("en-US", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
+  //create state to hold date and time
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
 
-  // get current time function HH:MM AM/PM
-  const time = new Date().toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  // useEffect hook to update date and time every second
+  useEffect(() => {
+    const updateDateTime = () => {
+      // get current date function MONTH DD, YYYY
+      const currentDate = new Date();
+
+      const formattedDate = currentDate.toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      });
+
+      // get current time function HH:MM AM/PM
+      const formattedTime = currentDate.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+
+      setDate(formattedDate);
+      setTime(formattedTime);
+    };
+
+    // call updateDateTime function every second
+    updateDateTime();
+    const interval = setInterval(updateDateTime, 1000);
+
+    // cleanup function to clear interval
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="system">
