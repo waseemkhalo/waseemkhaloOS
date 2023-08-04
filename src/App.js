a96c6fc4e486f564449ae3012d85a00979a426b7
Waseem <waseemkhalo@gmail.com>
2023-08-04 15:36:53 -0400

    add google analytics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Portfolio from "./pages/Portfolio/Portfolio";
 import * as React from "react";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
 
 function App() {
+  //add google analytics
+  useEffect(() => {
+    ReactGA.initialize("G-81EZR7G7KR");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}>
-          {" "}
-        </Route>
-        <Route path="/portfolio" element={<Portfolio />}>
           {" "}
         </Route>
       </Routes>
